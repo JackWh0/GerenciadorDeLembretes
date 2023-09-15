968322a1914a9b31a35498cd285452843ef8ca3b
Jackson Silva <jackson.silva.who@gmail.com>
2023-09-15 20:39:52 -0300

    Organizar listagem dos lembretes

--- gerenciadordelembretes/src/main/java/br/com/hallsdepapel/gerenciadordelembretes/repository/LembreteRepository.java
+++ gerenciadordelembretes/src/main/java/br/com/hallsdepapel/gerenciadordelembretes/repository/LembreteRepository.java
@@ -1,5 +1,6 @@
 package br.com.hallsdepapel.gerenciadordelembretes.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,6 @@ import br.com.hallsdepapel.gerenciadordelembretes.model.Lembrete;
 
 @Repository
 public interface LembreteRepository extends CrudRepository<Lembrete, Long>{
+    Iterable<Lembrete> findAll(Sort sort);
 
 }
