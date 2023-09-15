02c2531389dcec1449665bebca9daf5fad559420
Jackson Silva <jackson.silva.who@gmail.com>
2023-09-15 20:44:31 -0300

    JS para verificar campos e adicionar data padrão

--- gerenciadordelembretes/src/main/resources/static/js/form.js
+++ gerenciadordelembretes/src/main/resources/static/js/form.js
@@ -1,15 +1,24 @@
 const textoQuantidadeRestanteDescricao = document.getElementById('quantidadeRestanteDescricao');
 const textoQuantidadeRestanteTitulo = document.getElementById('quantidadeRestanteTitulo');
+const maxCaracteresTitulo = 27;
+const maxCaracteresDescricao = 255;
+let quantidadeCaracteresTitulo = 0;
+let quantidadeCaracteresDescricao = 0;
+let clicadoAnterior = '';
+let clicadoAtualmente = '';
 
-let contarQuantosCaracteresFaltam = (element, maxCaracteres) => {
-    let descricaoTextArea = element.value.length;
-    let resto = maxCaracteres - descricaoTextArea;
-    
-    if(element.id == 'descricao') {
+let contarQuantosCaracteresFaltam = (element) => {
+    let quantidadeCaracteres = element.value.length;
+
+    if (element.id == 'descricao') {
+        let resto = maxCaracteresDescricao - quantidadeCaracteres;
         textoQuantidadeRestanteDescricao.innerHTML = `Possui ${resto} caracteres restantes.`;
         limparMensagemCaracteresRestantes("titulo")
+        quantidadeCaracteresDescricao = quantidadeCaracteres;
     } else if (element.id == 'titulo') {
+        let resto = maxCaracteresTitulo - quantidadeCaracteres;
         textoQuantidadeRestanteTitulo.innerHTML = `Possui ${resto} caracteres restantes.`;
+        quantidadeCaracteresTitulo = quantidadeCaracteres;
         limparMensagemCaracteresRestantes("descricao")
     }
 }
@@ -17,7 +26,7 @@ let contarQuantosCaracteresFaltam = (element, maxCaracteres) => {
 let limparMensagemCaracteresRestantes = (element) => {
     switch (element) {
         case "titulo":
-            textoQuantidadeRestanteTitulo.innerHTML = "";        
+            textoQuantidadeRestanteTitulo.innerHTML = "";
             break;
         case "descricao":
             textoQuantidadeRestanteDescricao.innerHTML = "";
@@ -27,4 +36,42 @@ let limparMensagemCaracteresRestantes = (element) => {
             textoQuantidadeRestanteTitulo.innerHTML = "";
             break;
     }
+}
+
+let verificarSeCaracteresTituloExcedemPermitido =() => {
+    let excedeu = quantidadeCaracteresTitulo > maxCaracteresTitulo;
+    if(excedeu){
+        alert("Erro! Quantidade de caracteres no título ultrapassou o limite.\nLimite máximo: " + maxCaracteresTitulo + " caracteres.")
+    }
+    return excedeu;
+}
+
+let verificarSeCaracteresDescricaoExcedemPermitido = () => {
+    let excedeu = quantidadeCaracteresDescricao > maxCaracteresDescricao;
+    if(excedeu){
+        alert("Erro! Quantidade de caracteres na descrição ultrapassou o limite.\nLimite máximo: " + maxCaracteresDescricao + " caracteres.")
+    }
+    return excedeu;
+}
+
+let verificarSePodeEnviar = () => {
+    return !verificarSeCaracteresDescricaoExcedemPermitido() &&
+            !verificarSeCaracteresTituloExcedemPermitido();
+}
+
+let definirDataPadrao = () => {
+    let data = new Date();
+
+    let dia = data.getDate();
+    let mes = data.getMonth() + 1;
+    let ano = data.getFullYear();
+
+    if (mes < 10) {
+        mes = "0" + mes;
+    } if (dia < 10) {
+        dia = "0" + dia;
+    }
+    let hoje = ano + "-" + mes + "-" + dia;
+    document.getElementById("data").value = hoje;
+    document.getElementById("data").min = hoje;
 }
